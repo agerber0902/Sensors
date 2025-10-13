20eeba267b3345f2d151eddab8944fff900e1900
agerber0902 <agerber0902@gmail.com>
2025-10-13 10:52:31 -0400

    Initial Platform IO project create
    Created project in platform IO and no functionality

---
+++ ESP32_Moisture_Sensor/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+// Define Pins
+
+// Function Declarations:
+
+void setup() {
+  // Set Serial Rate
+
+  // Configure Pin Modes
+}
+
+void loop() {
+  
+}
+
+// Function Definitions:
+// TODO: will more than likely have separate files 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Sensors
 Repo for all sensor code
+
+Create ESP 32 env for Moisture Sensor using Platform IO
+
+Using Xiao ESP32 C3 using Arduino framework
