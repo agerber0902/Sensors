c9cb36333b2f0fda465b16de3f6f32758482ec9a
agerber0902 <agerber0902@gmail.com>
2025-10-21 17:29:40 -0400

    inital pir code with serial messaging and no led

--- Moition_Sensor/Motion_Sensor_Uno/src/main.cpp
+++ Moition_Sensor/Motion_Sensor_Uno/src/main.cpp
@@ -1,18 +1,59 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// define pins
+#define PIR_SENSOR_IN 5 // Digital input
+// #define MOTION_DETECTION_LED
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// CONSTANTS
+
+// Default values
+int state = LOW;
+
+void setup()
+{
+  // Set up Serial
+  Serial.begin(9600);
+  delay(1000);
+
+  Serial.println("Beginning Setup for UNO R4 PIR Motion Sensor...");
+
+  // Setup pin mode
+  pinMode(PIR_SENSOR_IN, INPUT);
+
+  Serial.println("Setup Complete.");
+  delay(1000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  Serial.println("Reading PIR Sensor values...");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Read pir value
+  int pir_value = digitalRead(PIR_SENSOR_IN);
+  Serial.println(pir_value);
+
+  if (pir_value == HIGH)
+  {
+    // digitalWrite(led, HIGH);
+    Serial.println("Alert");
+    delay(500); // Delay of led is 500
+    if (state == LOW)
+    {
+      Serial.println(" Motion detected");
+      state = HIGH;
+    }
+  }
+  else
+  {
+    // digitalWrite(led, LOW);
+    Serial.println("Non Alert");
+    delay(500);
+    if (state == HIGH)
+    {
+      Serial.println("The action/ motion has stopped");
+      state = LOW;
+    }
+  }
+
+  delay(5000);
 }
