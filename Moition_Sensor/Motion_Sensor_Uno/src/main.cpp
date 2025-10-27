7d637f0e3ddd31cbebed37337326cd4ed756712a
agerber0902 <agerber0902@gmail.com>
2025-10-27 10:39:37 -0400

    initial ultra sonic sensor code

--- Moition_Sensor/Motion_Sensor_Uno/src/main.cpp
+++ Moition_Sensor/Motion_Sensor_Uno/src/main.cpp
@@ -1,13 +1,22 @@
 #include <Arduino.h>
+#include <cstdio>
 
 // define pins
 #define PIR_SENSOR_IN 5 // Digital input
 // #define MOTION_DETECTION_LED
+#define ULTRA_SONIC_ECHO 2    // Digital Input for echo value
+#define ULTRA_SONIC_TRIGGER 3 // Digital Output to trigger ultra sonic read
 
 // CONSTANTS
 
-// Default values
+// Define variables
 int state = LOW;
+long ultra_sonic_duration; // Duration of the sound wave
+float distance, distance1, diff;
+
+// Define Functions
+void readPir();
+void readUltraSonicSensor();
 
 void setup()
 {
@@ -15,16 +24,52 @@ void setup()
   Serial.begin(9600);
   delay(1000);
 
-  Serial.println("Beginning Setup for UNO R4 PIR Motion Sensor...");
+  Serial.println("Beginning Setup for UNO R4 PIR and Ultra Sonic Motion Sensor...");
 
   // Setup pin mode
+
+  // Inputs
   pinMode(PIR_SENSOR_IN, INPUT);
+  pinMode(ULTRA_SONIC_ECHO, INPUT);
+
+  // Outputs
+  pinMode(ULTRA_SONIC_TRIGGER, OUTPUT);
+
+  // --------
 
   Serial.println("Setup Complete.");
   delay(1000);
 }
 
 void loop()
+{
+  readUltraSonicSensor();
+
+  delay(5000);
+}
+
+void readUltraSonicSensor()
+{
+  Serial.println("Reading Ultra Sonic Sensor values...");
+
+  digitalWrite(ULTRA_SONIC_TRIGGER, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRA_SONIC_TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRA_SONIC_TRIGGER, LOW);
+
+  ultra_sonic_duration = pulseIn(ULTRA_SONIC_ECHO, HIGH, 30000L); // wait up to 30ms
+  distance = ultra_sonic_duration * 0.034 / 2;
+
+  Serial.println(ultra_sonic_duration);
+  Serial.print("Distance (cm): ");
+  Serial.println(distance);
+
+  delay(1000);
+
+}
+
+void readPir()
 {
   Serial.println("Reading PIR Sensor values...");
 
@@ -54,6 +99,4 @@ void loop()
       state = LOW;
     }
   }
-
-  delay(5000);
 }
